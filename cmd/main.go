9c91510bb46ab552e525716053c354618726711c
home <home@me.com>
2023-05-21 16:47:10 +0300

    fix docs generation

--- cmd/main.go
+++ cmd/main.go
@@ -11,7 +11,7 @@ import (
 )
 
 // Provider documentation generation.
-//go:generate go run github.com/hashicorp/terraform-plugin-docs/cmd/tfplugindocs generate --provider-name gitea
+//go:generate go run github.com/hashicorp/terraform-plugin-docs/cmd/tfplugindocs generate --provider-name gitea --rendered-website-dir ../docs
 
 func main() {
 	providerserver.Serve(context.Background(), provider.New, providerserver.ServeOpts{
